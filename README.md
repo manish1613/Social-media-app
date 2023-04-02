7e2b9b4922d7ff1fed4ff1e9235e0267a5e10291
manish1613 <66303225+manish1613@users.noreply.github.com>
2023-04-02 15:57:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# WcCube Media
+
+![Wscube Media](https://i.ibb.co/c8v6v6t/Screenshot-2023-04-02-152932.png)
+
+## Introduction
+
+Using React, Node.js, Express & MongoDB i build a Full Stack MERN Application - from start to finish. The App is called "WsCube Media" and it is a simple social media app that allows users to post interesting events that happened in their lives.
+
+## Post form
+
+There user can fill their details and select only 1 image for post 
+
+![Post form](https://i.ibb.co/k0r8Trw/post.png)
+
+## Comment feature
+
+There only logged in user can comment on any post
+![Comment Page](https://i.ibb.co/TwWnL3p/comment.png)
+
+Setup:
+- run ```npm i && npm start``` for both client and server side to start the app
+
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Container, Grow, Grid, AppBar, TextField, Button, Paper } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import Pagination from '../Pagination';
+import useStyles from './styles';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+const Home = () => {
+  const classes = useStyles();
+  const query = useQuery();
+  const page = query.get('page') || 1;
+  
+
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+
+ 
+  const [tags, setTags] = useState([]);
+
+
+ 
+
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+          <Grid item xs={12} sm={6} md={9}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            {(!tags.length) && (
+              <Paper className={classes.pagination} elevation={6}>
+                <Pagination page={page} />
+              </Paper>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
